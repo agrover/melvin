b2fa9bb997ddbba8ece711823d0bfdb96e53dfcd
Andy Grover <agrover@redhat.com>
2015-06-25 17:25:12 -0700

    more fun test code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#![feature(iter_arith)]
-#![feature(collections)]
+#![feature(iter_arith, result_expect)]
 
 extern crate byteorder;
 extern crate crc;
@@ -11,6 +10,9 @@ use std::path;
 use std::io::Result;
 use std::io::Error;
 use std::io::ErrorKind::Other;
+
+use nix::fcntl;
+use std::os::unix::io::AsRawFd;
 
 mod parser;
 mod lvmetad;
@@ -24,6 +26,7 @@ mod pv;
 mod dm_ioctl;
 
 use parser::LvmTextMap;
+use parser::TextMapOps;
 
 fn get_first_vg_meta() -> Result<(String, LvmTextMap)> {
     let dirs = vec![path::Path::new("/dev")];
@@ -42,15 +45,33 @@ fn get_first_vg_meta() -> Result<(String, LvmTextMap)> {
     Err(Error::new(Other, "dude"))
 }
 
+fn get_conf() -> Result<LvmTextMap> {
+    use std::fs;
+    use std::io::Read;
+
+    let mut f = try!(fs::File::open("/etc/lvm/lvm.conf"));
+
+    let mut buf = Vec::new();
+    try!(f.read_to_end(&mut buf));
+
+    parser::into_textmap(&buf)
+}
+
 fn main() {
     // println!("A");
-    // let (name, mut map) = get_first_vg_meta().unwrap();
+    let (name, mut map) = get_first_vg_meta().unwrap();
     // println!("B");
-    // let vg = parser::vg_from_textmap(&name, &mut map);
+    let vg = parser::vg_from_textmap(&name, &mut map).expect("didn't get vg!");
+    println!("heyo {}", vg.extents_in_use());
     // println!("output {:?}", vg);
 
-    match dm::list_devices() {
-        Ok(x) => println!("{:?}", x),
-        Err(x) => println!("error {}", x),
-    }
+    // match dm::list_devices() {
+    //     Ok(x) => println!("{:?}", x),
+    //     Err(x) => println!("error {}", x),
+    // }
+
+    let tm = get_conf().unwrap();
+    let global = tm.textmap_from_textmap("global").unwrap();
+
+    println!("config = {:?}", global.i64_from_textmap("locking_type").unwrap());
 }

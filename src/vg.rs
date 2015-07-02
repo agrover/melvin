ce318a248dbb272cf9d2c9e537b6fac5ce71e2eb
Andy Grover <agrover@redhat.com>
2015-07-02 11:18:02 -0700

    implement vg used_ranges and free_ranges

--- src/vg.rs
+++ src/vg.rs
@@ -36,4 +36,42 @@ impl VG {
             .map(|x| x.pe_count)
             .sum()
     }
+
+    pub fn used_ranges(&self) -> BTreeMap<String, BTreeMap<u64, u64>> {
+        let mut used_map = BTreeMap::new();
+
+        // pretty sure this is only correct for my system...
+        for (lvname, lv) in &self.lvs {
+            for seg in &lv.segments {
+                for &(ref pvname, start) in &seg.stripes {
+                    used_map.entry(pvname.to_string()).or_insert(BTreeMap::new())
+                        .insert(start as u64, seg.extent_count);
+                }
+            }
+        }
+
+        used_map
+    }
+
+    pub fn free_ranges(&self) -> BTreeMap<String, BTreeMap<u64, u64>> {
+        let mut free_map = BTreeMap::new();
+
+        for (pvname, area_map) in &mut self.used_ranges() {
+
+            // Insert an entry to mark the end of the PV so the fold works correctly
+            let pv = self.pvs.get(pvname).expect("area map name refers to nonexistent PV");
+            area_map.insert(pv.pe_count, 0);
+
+            area_map.iter()
+                .fold(0, |prev_end, (start, len)| {
+                    if prev_end < *start {
+                        free_map.entry(pvname.clone()).or_insert(BTreeMap::new())
+                            .insert(prev_end, start-prev_end);
+                    }
+                    start + len
+                });
+        }
+
+        free_map
+    }
 }

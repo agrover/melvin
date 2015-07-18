2c5a50c12e6755d7e02409ff992422692b45d9f4
Andy Grover <agrover@redhat.com>
2015-07-17 17:52:52 -0700

    dm: Create a DM object
    Put almost all the dm functions in the DM object so we just have to open
    the /dev/mapper/control file once.
    
    struct DM contains a VG ref so it can calculate stuff.

--- src/dm.rs
+++ src/dm.rs
@@ -1,13 +1,19 @@
 use std::fs;
+use std::fs::File;
 use std::io;
 use std::os::unix::io::AsRawFd;
 use std::mem;
+use std::slice::bytes::copy_memory;
+use std::io::Error;
+use std::io::ErrorKind::Other;
 
 use byteorder::{LittleEndian, ByteOrder};
 
 use nix::sys::ioctl;
 
 use dm_ioctl as dmi;
+use lv::LV;
+use vg::VG;
 
 const DM_IOCTL: u8 = 0xfd;
 const DM_CTL_PATH: &'static str= "/dev/mapper/control";
@@ -16,24 +22,159 @@ const DM_VERSION_MAJOR: u32 = 4;
 const DM_VERSION_MINOR: u32 = 30;
 const DM_VERSION_PATCHLEVEL: u32 = 0;
 
-pub fn get_version() -> io::Result<(u32, u32, u32)> {
+pub struct DM<'a> {
+    file: File,
+    vg: &'a VG,
+}
 
-    let f = try!(fs::File::open(DM_CTL_PATH));
+impl <'a> DM<'a> {
+    pub fn new(vg: &'a VG) -> io::Result<Self> {
+        Ok(DM {
+            file: try!(File::open(DM_CTL_PATH)),
+            vg: vg,
+        })
+    }
 
-    let mut hdr: dmi::Struct_dm_ioctl = Default::default();
-    hdr.version[0] = DM_VERSION_MAJOR;
-    hdr.version[1] = DM_VERSION_MINOR;
-    hdr.version[2] = DM_VERSION_PATCHLEVEL;
+    fn get_version(&self) -> io::Result<(u32, u32, u32)> {
 
-    let op = ioctl::op_read_write(DM_IOCTL, dmi::DM_VERSION_CMD as u8,
-                            mem::size_of::<dmi::Struct_dm_ioctl>());
+        let mut hdr: dmi::Struct_dm_ioctl = Default::default();
+        hdr.version[0] = DM_VERSION_MAJOR;
+        hdr.version[1] = DM_VERSION_MINOR;
+        hdr.version[2] = DM_VERSION_PATCHLEVEL;
 
-    match unsafe { ioctl::read_into(f.as_raw_fd(), op, &mut hdr) } {
-        Err(_) => return Err((io::Error::last_os_error())),
-        _ => {},
-    };
+        let op = ioctl::op_read_write(DM_IOCTL, dmi::DM_VERSION_CMD as u8,
+                                      mem::size_of::<dmi::Struct_dm_ioctl>());
 
-    Ok((hdr.version[0], hdr.version[1], hdr.version[2]))
+        match unsafe { ioctl::read_into(self.file.as_raw_fd(), op, &mut hdr) } {
+            Err(_) => return Err((io::Error::last_os_error())),
+            _ => {},
+        };
+
+        Ok((hdr.version[0], hdr.version[1], hdr.version[2]))
+    }
+
+    fn list_devices(&self) -> io::Result<Vec<(String, u64)>> {
+
+        let mut devs = Vec::new();
+
+        let mut buf = [0u8; 10240];
+
+        let hdr: &mut dmi::Struct_dm_ioctl = unsafe {mem::transmute(&mut buf)};
+
+        let hdr_size = mem::size_of::<dmi::Struct_dm_ioctl>();
+        hdr.version[0] = DM_VERSION_MAJOR;
+        hdr.version[1] = DM_VERSION_MINOR;
+        hdr.version[2] = DM_VERSION_PATCHLEVEL;
+        hdr.data_size = buf.len() as u32;
+        hdr.data_start = hdr_size as u32;
+
+        let op = ioctl::op_read_write(DM_IOCTL, dmi::DM_LIST_DEVICES_CMD as u8, buf.len());
+
+        match unsafe { ioctl::read_into(self.file.as_raw_fd(), op, &mut buf) } {
+            Err(_) => return Err((io::Error::last_os_error())),
+            _ => {},
+        };
+
+        if (hdr.data_size - hdr_size as u32) != 0 {
+            let mut result = &buf[hdr_size..];
+
+            loop {
+                let slc = slice_to_null(&result[12..]).expect("Bad data from ioctl");
+                let devno = LittleEndian::read_u64(&result[..8]);
+                devs.push((String::from_utf8_lossy(slc).into_owned(), devno));
+
+                let next = LittleEndian::read_u32(&result[8..12]);
+                if next == 0 { break }
+
+                result = &result[next as usize..];
+            }
+        }
+
+        Ok(devs)
+    }
+
+    fn initialize_hdr(lv: &LV, hdr: &mut dmi::Struct_dm_ioctl) -> () {
+        hdr.version[0] = DM_VERSION_MAJOR;
+        hdr.version[1] = DM_VERSION_MINOR;
+        hdr.version[2] = DM_VERSION_PATCHLEVEL;
+
+        // Transmute [i8; 128] to [u8; 128]
+        let name_dest: &mut [u8; 128] = unsafe { mem::transmute(&mut hdr.name) };
+        copy_memory(&lv.name.as_bytes(), &mut name_dest[..]);
+
+        let uuid_dest: &mut [u8; 129] = unsafe { mem::transmute(&mut hdr.uuid) };
+        copy_memory(&lv.id.as_bytes(), &mut uuid_dest[..]);
+    }
+
+    fn create_device(&self, lv: &LV) -> io::Result<()> {
+        let mut hdr: dmi::Struct_dm_ioctl = Default::default();
+
+        Self::initialize_hdr(lv, &mut hdr);
+
+        let op = ioctl::op_read_write(DM_IOCTL, dmi::DM_DEV_CREATE_CMD as u8,
+                                      mem::size_of::<dmi::Struct_dm_ioctl>());
+
+        match unsafe { ioctl::read_into(self.file.as_raw_fd(), op, &mut hdr) } {
+            Err(_) => return Err((io::Error::last_os_error())),
+            _ => Ok(())
+        }
+    }
+
+    fn load_device(&self, lv: &LV) -> io::Result<()> {
+        let mut buf = [0u8; 10240];
+
+        println!("hihi");
+
+        let mut hdr: &mut dmi::Struct_dm_ioctl = unsafe {mem::transmute(&mut buf)};
+
+        let hdr_size = mem::size_of::<dmi::Struct_dm_ioctl>();
+
+        Self::initialize_hdr(lv, &mut hdr);
+
+        hdr.data_size = buf.len() as u32;
+        hdr.data_start = hdr_size as u32;
+
+        println!("hihi2");
+
+        //let op = ioctl::op_read_write(DM_IOCTL, dmi::DM_LIST_DEVICES_CMD as u8, buf.len());
+
+        let err = || Error::new(Other, "dm load_device error");
+
+        let sectors_per_extent = self.vg.extent_size;
+
+        let data = &buf[hdr_size..];
+        for seg in &lv.segments {
+            for &(ref pvname, ref loc) in &seg.stripes {
+                let pv = try!(self.vg.pvs.get(pvname).ok_or(err()));
+
+                println!("{} {} linear {}:{} {}",
+                         seg.start_extent * sectors_per_extent,
+                         seg.extent_count * sectors_per_extent,
+                         pv.device.major,
+                         pv.device.minor,
+                         (loc * sectors_per_extent) + pv.pe_start);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn resume_device(&self, lv: &LV) -> io::Result<()> {
+        Ok(())
+    }
+
+    pub fn activate_device(&self, lv: &LV) -> io::Result<()> {
+
+        // TODO: name/uuid mangle?
+
+//        self.create_device(&lv);
+
+        self.load_device(&lv);
+
+        self.resume_device(&lv);
+
+        Ok(())
+    }
 }
 
 //
@@ -44,46 +185,4 @@ fn slice_to_null(slc: &[u8]) -> Option<&[u8]> {
         if *c == b'\0' { return Some(&slc[..i]) };
     }
     None
-}
-
-pub fn list_devices() -> io::Result<Vec<(String, u64)>> {
-
-    let mut devs = Vec::new();
-
-    let f = try!(fs::File::open(DM_CTL_PATH));
-
-    let mut buf = [0u8; 10240];
-
-    let hdr: &mut dmi::Struct_dm_ioctl = unsafe {mem::transmute(&mut buf)};
-
-    let hdr_size = mem::size_of::<dmi::Struct_dm_ioctl>();
-    hdr.version[0] = DM_VERSION_MAJOR;
-    hdr.version[1] = DM_VERSION_MINOR;
-    hdr.version[2] = DM_VERSION_PATCHLEVEL;
-    hdr.data_size = buf.len() as u32;
-    hdr.data_start = hdr_size as u32;
-
-    let op = ioctl::op_read_write(DM_IOCTL, dmi::DM_LIST_DEVICES_CMD as u8, buf.len());
-
-    match unsafe { ioctl::read_into(f.as_raw_fd(), op, &mut buf) } {
-        Err(_) => return Err((io::Error::last_os_error())),
-        _ => {},
-    };
-
-    if (hdr.data_size - hdr_size as u32) != 0 {
-        let mut result = &buf[hdr_size..];
-
-        loop {
-            let slc = slice_to_null(&result[12..]).expect("Bad data from ioctl");
-            let devno = LittleEndian::read_u64(&result[..8]);
-            devs.push((String::from_utf8_lossy(slc).into_owned(), devno));
-
-            let next = LittleEndian::read_u32(&result[8..12]);
-            if next == 0 { break }
-
-            result = &result[next as usize..];
-        }
-    }
-
-    Ok(devs)
 }

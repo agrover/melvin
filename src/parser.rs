8cda6d86d5334fd3d4656f78718c06085b3082b9
Andy Grover <agrover@redhat.com>
2015-07-27 17:20:51 -0700

    License under the MPL 2.0
    To me, this is the best license to use with Rust libraries that still
    retains a copyleft aspect. All we want is any changes to Melvin to be
    contributed back, and allow an unmodified Melvin to be statically linked
    without triggering copyleft.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
+// This code is based on https://github.com/Byron/json-tools .
 // Copyright © 2015 Sebastian Thiel
-// Copyright © 2015 Andy Grover
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -11,18 +11,14 @@
 //
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
+
+// Copyright © 2015 Andy Grover
 //
-// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // Base a lexer for LVM2's text format on the more complex (hah) json format.
-// This code is based on https://github.com/Byron/json-tools.
-
 //
 // Given a &[u8], the lexer produces a stream of tokens.
 // get_textmap takes tokens and produces a TextMap nested

6faaf7668e0dbf03c298fe87de9a4b95948640b1
Andy Grover <agrover@redhat.com>
2015-07-30 17:18:42 -0700

    Update doc.

---
+++ doc/crc/crc32/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["CASTAGNOLI",""],["IEEE",""],["KOOPMAN",""]],"fn":[["checksum_castagnoli",""],["checksum_ieee",""],["checksum_koopman",""],["make_table",""],["update",""]],"struct":[["Digest",""]],"trait":[["Hasher32",""]]});
--- doc/melvin/pvlabel/sidebar-items.js
+++ doc/melvin/pvlabel/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["scan_for_pvs","Scan a list of directories for block devices containing LVM PV labels."]],"struct":[["MDA","A handle to an LVM on-disk metadata area (MDA)"]]});
+initSidebarItems({"fn":[["scan_for_pvs","Scan a list of directories for block devices containing LVM PV labels."]],"struct":[["PvArea","Describes an area within a PV"],["PvHeader","A struct containing the values in the PV header. It contains pointers to the data area, and possibly metadata areas and bootloader area."]]});
